482913d64082446c07bfc78fbd600344c2ba1a8a
Wadood Hussaini <wadood@webflow.com>
2023-03-30 13:11:14 -0400

    first POF working

---
+++ dist/bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+var form = document.querySelector("#form");
+form.style.backgroundColor = "pink";
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
