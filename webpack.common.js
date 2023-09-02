0256affd6a28f2a92e5a87a433cd09ec0d6f68fc
Wadood Hussaini <wadood@webflow.com>
2023-09-02 17:22:22 -0400

    change webpack prod config

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,21 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const glob = require("glob");
 
 module.exports = {
   entry: {
     index: "./src/index.js",
   },
-  output: {
-    // path: path.resolve(__dirname, "dist"),
+  /* output: {
+    path: path.resolve(__dirname, "dist"),
     filename: "[name].bundle.js",
     clean: true,
+  }, */
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    clean: true,
+    assetModuleFilename: "[name][ext]",
   },
   plugins: [
     new HtmlWebpackPlugin({
