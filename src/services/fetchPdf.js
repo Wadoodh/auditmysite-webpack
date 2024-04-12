1a5096b835a7889924da03f4316101f30915195e
Wadood Hussaini <wadood@webflow.com>
2024-04-12 11:43:51 -0400

    restore autocode url

--- src/services/fetchPdf.js
+++ src/services/fetchPdf.js
@@ -7,8 +7,10 @@ export default async function fetchPdf() {
   console.log(html);
 
   // https://dev--make-pdf--webflow-success.autocode.dev/
+  // https://6zci2ewwouoxbt6mh2csp25zni0vymdp.lambda-url.us-east-2.on.aws/
+
   const { data } = await axios.post(
-    "https://6zci2ewwouoxbt6mh2csp25zni0vymdp.lambda-url.us-east-2.on.aws/",
+    "https://dev--make-pdf--webflow-success.autocode.dev/",
     { markup: html }
   );
 
