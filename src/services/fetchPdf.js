a48db8bb079496977b68844db62102fe640de827
Wadood Hussaini <wadood@webflow.com>
2024-04-12 12:01:06 -0400

    aws for pdf working

--- src/services/fetchPdf.js
+++ src/services/fetchPdf.js
@@ -4,13 +4,11 @@ import getHtmlForPdf from "../render/getHtmlForPdf";
 export default async function fetchPdf() {
   const html = getHtmlForPdf();
 
-  console.log(html);
-
   // https://dev--make-pdf--webflow-success.autocode.dev/
   // https://6zci2ewwouoxbt6mh2csp25zni0vymdp.lambda-url.us-east-2.on.aws/
 
   const { data } = await axios.post(
-    "https://dev--make-pdf--webflow-success.autocode.dev/",
+    "https://6zci2ewwouoxbt6mh2csp25zni0vymdp.lambda-url.us-east-2.on.aws/",
     { markup: html }
   );
 
